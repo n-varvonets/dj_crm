e19d7dc8d70266326d9e520e5f1deba6183c2ef8
n-varvonets <nickolay.varvonets@gmail.com>
2021-08-03 18:22:16 +0300

    added ModelForm instead usual Form. method save(), class Meta: .... and display this relation O2M(Agent)

--- leads/forms.py
+++ leads/forms.py
@@ -1,8 +1,22 @@
 from django import forms
+from .models import Lead
+
+
+class LeadModelForm(forms.ModelForm):
+    """if we want to display fields from model including relations O2M, M2M"""
+    class Meta:  # in MEta we specify information about the form
+        model = Lead
+        """ specify the fields which we want to display in actual form from model"""
+        fields = (
+            "first_name",
+            "last_name",
+            "age",
+            "agent",
+        )
 
 
 class LeadForm(forms.Form):
-    """inpute inside the fields which has to recieve the forms"""
+    """inpute inside the fields which has to recieve the forms(standart form without O2M)"""
     first_name = forms.CharField()
     last_name = forms.CharField()
     age = forms.IntegerField(min_value=0)
--- leads/views.py
+++ leads/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Lead, Agent
-from .forms import LeadForm
+from .forms import LeadForm, LeadModelForm
 # Create your views here.
 
 
 def lead_list(request):
     # 2) с помощью  queryset  получаем наши обьекты из модели Lead и передаеим в конткест
-
     leads = Lead.objects.all()
     # 1)можно третьим параметром передавать в темплейты контекст(наши переменные)  и потом во вьюхах их принимать {{_}}
     context = {
@@ -28,31 +27,46 @@ def lead_detail(request, pk):
 
 
 def lead_create(request):
-    form = LeadForm()  # при новом посте создаем новый его инстанс
-    # print(request.POST)  # <QueryDict: {'csrfmiddlewaretoken': ['D98dwOAYBGvMEkoOsedEU9jyZuFtpBiUdweNJ3sf7rQbUVrE4r9SOcTHyLrsWQKu'], 'first_name': ['First Name'], 'last_name': ['Last Name'], 'age': ['213']}>
+    """create form page with LeadModelForm with fields relation of O2M(Agent), M2M and using save() func. Just compared with  regular Form"""
+    form = LeadModelForm()
     if request.method == "POST":
-        print("Receiving a post request")
-        form = LeadForm(request.POST)
-        # print('form is ..', form)  >>> <tr><th><label for="id_first_name">First name:</label></th><td><input type="text" name="first_name" value="21"
+        form = LeadModelForm(request.POST)
         if form.is_valid():
-            print("The form is valid")
-            print(form.cleaned_data)  #{'first_name': 'form name', 'last_name': 'form l_name', 'age': 87}
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            age = form.cleaned_data['age']
-            agent = Agent.objects.first()  #  для примера возьмем первого агента(requiered field для создания лида)
-
-            # собсна и создает его...
-            Lead.objects.create(
-                first_name=first_name,
-                last_name=last_name,
-                age=age,
-                agent=agent
-            )
-            print('The lead has been created')
+            """Django ModelForm allows to save this form"""
+            form.save()  # instead standart Form
             return redirect("/leads")
     context = {
         "form": form
     }
     return render(request, "leads/lead_create.html", context)
+
+
+# def lead_create(request):
+#     form = LeadForm()  # при новом посте создаем новый его инстанс
+#     # print(request.POST)  # <QueryDict: {'csrfmiddlewaretoken': ['D98dwOAYBGvMEkoOsedEU9jyZuFtpBiUdweNJ3sf7rQbUVrE4r9SOcTHyLrsWQKu'], 'first_name': ['First Name'], 'last_name': ['Last Name'], 'age': ['213']}>
+#     if request.method == "POST":
+#         print("Receiving a post request")
+#         form = LeadForm(request.POST)
+#         # print('form is ..', form)  >>> <tr><th><label for="id_first_name">First name:</label></th><td><input type="text" name="first_name" value="21"
+#         if form.is_valid():
+#             print("The form is valid")
+#             print(form.cleaned_data)  #{'first_name': 'form name', 'last_name': 'form l_name', 'age': 87}
+#             first_name = form.cleaned_data['first_name']
+#             last_name = form.cleaned_data['last_name']
+#             age = form.cleaned_data['age']
+#             agent = Agent.objects.first()  #  для примера возьмем первого агента(requiered field для создания лида)
+#
+#             # собсна и создает его...
+#             Lead.objects.create(
+#                 first_name=first_name,
+#                 last_name=last_name,
+#                 age=age,
+#                 agent=agent
+#             )
+#             print('The lead has been created')
+#             return redirect("/leads")
+#     context = {
+#         "form": form
+#     }
+#     return render(request, "leads/lead_create.html", context)
 
