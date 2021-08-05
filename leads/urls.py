ff2187673c71e94ffd55c37b5cd2149cea3bc1dc
n-varvonets <nickolay.varvonets@gmail.com>
2021-08-05 12:00:24 +0300

    cleared comments in url.py and view.py cause of adding generic views

--- leads/urls.py
+++ leads/urls.py
@@ -3,31 +3,14 @@
 # by default using from main apps(djcrm) urls which indicated in setting
 
 from django.urls import path
-from .views import lead_list, lead_detail, lead_create, lead_update, lead_delete, LeadListView, LeadDetailView, LeadCreateView, LeadUpdateView, LeadDeleteView
+from .views import *
 
-# уникальный индекфикатор урлдов нашего app, который был получен из djcrm/urls.py
 app_name = "leads"
 
-
-
-"""this is hardocoding not using URL namespace.. and if I decide to change the url - it also necessary to change in ALL PLACES in templates whenewere I were referenced at them"""
-# urlpatterns = [
-#     path('', lead_list),
-#     path('<int:pk>/', lead_detail),  # pk - это уникальный айди записи в таблице, по кторому мы можем достучаться к конкретной записи
-#     path('<int:pk>/update/', lead_update),
-#     path('<int:pk>/delete/', lead_delete),
-#     path('create/', lead_create),
-# ]
-
 urlpatterns = [
-    # path('', lead_list, name='lead-list'),
     path('', LeadListView.as_view(), name='lead-list'),
-    # path('<int:pk>/', lead_detail, name='lead-detail'),  # pk - это уникальный айди записи в таблице, по кторому мы можем достучаться к конкретной записи
-    path('<int:pk>/', LeadDetailView.as_view(), name='lead-detail'),  # pk - это уникальный айди записи в таблице, по кторому мы можем достучаться к конкретной записи
-    # path('<int:pk>/update/', lead_update, name='lead-update'),
+    path('<int:pk>/', LeadDetailView.as_view(), name='lead-detail'),
     path('<int:pk>/update/', LeadUpdateView.as_view(), name='lead-update'),
-    # path('<int:pk>/delete/', lead_delete, name='lead-delete'),
     path('<int:pk>/delete/', LeadDeleteView.as_view(), name='lead-delete'),
-    # path('create/', lead_create, name='lead-create'),
     path('create/', LeadCreateView.as_view(), name='lead-create'),
 ]
