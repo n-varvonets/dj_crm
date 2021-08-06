47d0c1bd278bb1ad43ce0e329407b64fc750e4ad
n-varvonets <nickolay.varvonets@gmail.com>
2021-08-06 17:03:13 +0300

    added agent.Detail and agent.Delete and View

--- agents/views.py
+++ agents/views.py
@@ -28,3 +28,25 @@ class AgentCreateView(LoginRequiredMixin, generic.CreateView):
         agent.organization = self.request.user.userprofile
         agent.save()  # а теперь с атрибутом organization можем сохранить только что созданную запись агента
         return super(AgentCreateView, self).form_valid(form)
+
+
+class AgentDetailView(LoginRequiredMixin, generic.DetailView):
+    template_name = "agents/agent_detail.html"
+    context_object_name = 'agent'  # for template to indicate our context
+
+    def get_queryset(self):
+        return Agent.objects.all()
+
+
+class AgentUpdateView(LoginRequiredMixin, generic.UpdateView):
+    template_name = "agents/agent_update.html"
+    form_class = AgentModelForm
+
+    def get_success_url(self):
+        return reverse("agents:agent-list")
+
+    def get_queryset(self):
+        return Agent.objects.all()
+
+# class AgentDeleteView(LoginRequiredMixin, generic.DeleteView):
+#
