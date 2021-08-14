7fbf624c058d5e2ff26256863aa32a67f74c8206
n-varvonets <nickolay.varvonets@gmail.com>
2021-08-14 14:52:22 +0300

    added in setting.py if not DEBUG and gunicorn

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -15,7 +15,6 @@ import environ
 
 
 env = environ.Env(
-    #
     DEBUG=(bool, False)
 )
 # Но в проде мы его не будем использовать, по тому что .env не хранится в репозитории(желтым цветом для гита).
@@ -183,3 +182,17 @@ LOGOUT_REDIRECT_URL = '/'
 # for ability to use Crispy  forms
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'tailwind'
 CRISPY_TEMPLATE_PACK = 'tailwind'
+
+if not DEBUG:
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_HSTS_SECONDS = 31536000  # 1 year
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    X_FRAME_OPTIONS = "DENY"
+
+    ALLOWED_HOSTS = ["*"]
