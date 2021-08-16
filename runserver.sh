bd28f702a85a2c9184707521fbfca7bd41758187
n-varvonets <nickolay.varvonets@gmail.com>
2021-08-16 14:22:22 +0300

    prepared emailing for production app

--- runserver.sh
+++ runserver.sh
@@ -2,7 +2,7 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-gunicorn --worker-tmp-dir /dev/shm djcrm.wsgi
+gunicorn --worker-tmp-dir /dev/shm djcrm.wsgi  # gunicorn djcrm.wsgi:application -  this is the same as "python manage.py runserver" - but that's how run the app in production of DigitalOcean
 
 # chmod +x runserver.sh
 # ./runserver.sh
